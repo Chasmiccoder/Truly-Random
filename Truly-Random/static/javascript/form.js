41020dc8aadb9bb62a20b6de8cc686f2443380d3
Aryaman Kolhe <chasmiccoder@gmail.com>
2021-08-04 23:36:48 +0530

    Form Failure

--- Truly-Random/static/javascript/form.js
+++ Truly-Random/static/javascript/form.js
@@ -5,11 +5,38 @@ function increment() {
 }
 var previous = ""; // prevents additional dialogue boxes from being created
 // find a better way of doing this instead of using global vars
+var numbers = [];
+
+// Put these following lines in a function?
+// const formWrapper = document.querySelector('.formWrapper');
+// const form = formWrapper.querySelectorAll('.formClass');
+// const submitInput = form[0].querySelector('input[type="submit"]');
+
+// // Parameter e is an event
+// function getDataForm(e) {
+//     e.preventDefault(); // prevent default submit behaviour on clicking the button
+//     console.log("lknfknlq");
+//     // looks through all the form fields and collects the data present in them
+//     var formData = new FormData(form[0]);
+
+//     alert( formData.get('lowerLimitField') + ' - ' + formData.get('upperLimitField') );
+
+// }
+
+// Create an event listener that will trigger getDataForm(e)
+// document.addEventListener('DOMContentLoaded', function(){
+//     console.log("REACHED");
+//     submitInput.addEventListener( 'click', getDataForm, false ); 
+//  }, false);
+ 
+
+
 
 
 function getOperation() {
     var e = document.getElementById("operation_select").value;
-    var number = 0; // Random Number Generated
+    var num = 0; // Random Number Generated
+    // change variable name of e
 
     if ( previous == e ) {
         // something
@@ -21,56 +48,55 @@ function getOperation() {
         // clear function that deletes previous fields
 
         if ( e == "coin_toss" ) {
-            number = fetchCoinToss();
+            num = fetchCoinToss();
         }
 
         else if ( e == "integer" ) {
-            number = fetchInteger();
+            num = fetchInteger();
         }
 
         else if ( e == "float" ) {
-
+            num = fetchFloat();
         }
 
         else if ( e == "n_digit_int" ) {
-
+            num = fetchNDigitInt();
         }
 
         else if ( e == "fraction" ) {
-
+            num = fetchFraction();
         }
 
         else if ( e == "n_digit_binary" ) {
-
+            num = fetchNDigitBinary();
         }
-    }
-    previous = e;
-
-    /*
-    <form action="#" id="mainform" method="get" name="mainform">
-                <span id="myForm"></span>
-                <p></p><input type="submit" value="Generate!">
-                </form>
-
-*/
-    var r = document.createElement('span');
-    // submit
-    var sub = document.createElement("INPUT");
-    sub.setAttribute("type", "submit");
-    sub.setAttribute("value", "Generate!");
-    increment();
-    sub.setAttribute("Name", "textelement_" + i); // what is this
-
-    r.appendChild(sub);
-    r.setAttribute("id", "id_" + i);
-    document.getElementById("mainform").appendChild(r);
     
+        previous = e;
+
+        
+        // var r = document.createElement('span');
+        // // submit
+        // var sub = document.createElement("INPUT");
+        // sub.setAttribute("type", "submit");
+        // sub.setAttribute("value", "Generate!");
+        // increment();
+        // sub.setAttribute("name", "textelement_" + i);
+
+        // r.appendChild(sub);
+
+        // increment()
+        // r.setAttribute("id", "id_" + i);
+        // document.getElementById("mainform").appendChild(r);
+
+
+        numbers += [num];
+    }
 
 }
 
 
 function fetchCoinToss() {
-    return 1;
+    return 1000;
 }
 
 
@@ -85,13 +111,15 @@ function fetchInteger() {
 
     lower_limit.setAttribute("type", "text");
     lower_limit.setAttribute("placeholder", "Lower Limit"); // Lower limit
-    lower_limit.setAttribute("Name", "textelement_" + i);
+    lower_limit.setAttribute("name", "textelement_" + i);
+    lower_limit.setAttribute("id", "lowerLimitField");
     
-    // increment()
+    increment()
 
     upper_limit.setAttribute("type", "text");
     upper_limit.setAttribute("placeholder", "Upper Limit");
-    upper_limit.setAttribute("Name", "textelement_" + i)
+    upper_limit.setAttribute("name", "textelement_" + i);
+    upper_limit.setAttribute("id", "upperLimitField");
     
     
     r.appendChild(lower_limit);
@@ -100,16 +128,54 @@ function fetchInteger() {
 
     document.getElementById("mainform").appendChild(r);
 
+
+    var r = document.createElement('span');
+    // submit
+    var sub = document.createElement("BUTTON");
+    // sub.setAttribute("type", "button");
+    // sub.setAttribute("value", "Generate!");
+    sub.innerHTML = "Generate!";
+    increment();
+    sub.setAttribute("name", "textelement_" + i);
+    sub.setAttribute("onClick", "displayNumber");
+
+    r.appendChild(sub);
+
+    increment()
+    r.setAttribute("id", "id_" + i);
+    document.getElementById("mainform").appendChild(r);
+
+
     
 
 
-    return 0;
+    return 2000;
+}
+
+function fetchFloat() {
+    return 3000;
+}
+
+function fetchNDigitInt() {
+    return 4000;
+}
+
+function fetchFraction() {
+    return 5000;
+}
+
+function fetchNDigitBinary() {
+    return 6000;
 }
 
 
 
 
+function displayNumber() {
+    var ll = document.getElementById('lowerLimitField').value;
+    var ul = document.getElementById('upperLimitField').value;
 
-
-
-
+    tmp = (ll+ul)/2;
+    // outputHere
+    document.getElementById('outputHere').value = tmp;
+}
